819e348abc0dc0b46b0e837acbd10051c281db1a
x-finity <x.finity777@gmail.com>
2024-03-01 22:52:11 +0300

    add comments on task number

--- 04-dml/2_3_4_tasks_select.sql
+++ 04-dml/2_3_4_tasks_select.sql
@@ -1,47 +1,57 @@
 /* 2 task */
+-- 2.1
 SELECT title, duration 
 	FROM public.tracks
 	WHERE duration = (SELECT max(duration) FROM public.tracks);
 
+-- 2.2
 SELECT title
 	FROM public.tracks
 	WHERE duration >= 230;
 
+-- 2.3
 SELECT title
 	FROM public.collections
 	WHERE release_year BETWEEN 2018 AND 2020;
 
+-- 2.4
 SELECT "name"
 	FROM public.artists
 	WHERE "name" NOT LIKE '% %';
 
+-- 2.5
 SELECT title 
 	FROM public.tracks
 	WHERE (title LIKE '%my%' OR title LIKE '%мой%');
 
 /* 3 task */
+-- 3.1
 SELECT COUNT(artist_id), "name"
 	FROM public.artistsgenre ag
 	LEFT JOIN genres g ON ag.genre_id = g.id 
 	GROUP BY "name";
 
+-- 3.2
 SELECT COUNT(*), release_year
 	FROM public.tracks t 
 	JOIN albums a ON t.album_id = a.id
 	WHERE release_year BETWEEN 2019 AND 2020
 	GROUP BY release_year;
 
+-- 3.3
 SELECT AVG(duration), al.title
 	FROM public.tracks t 
 	LEFT JOIN albums al ON t.album_id = al.id 
 	GROUP BY al.title;
 
+-- 3.4
 SELECT a.name, release_year
 	FROM public.artistsalbum aa
 	JOIN artists a ON aa.artist_id = a.id
 	JOIN albums al ON aa.album_id = al.id
 	WHERE release_year != 2020;
 
+-- 3.5
 SELECT c.title, a.name
 	FROM public.collectionstracks ct
 	JOIN collections c ON ct.collection_id = c.id 
@@ -52,6 +62,7 @@ SELECT c.title, a.name
 	WHERE a."name" = 'Queen';
 
 /* 4 task */
+-- 4.1
 SELECT al.title
 	FROM public.albums al
 	JOIN artistsalbum aa ON al.id = aa.album_id 
@@ -62,11 +73,13 @@ SELECT al.title
 	HAVING COUNT(g.name) > 1
 	ORDER BY al.title;
 
+-- 4.2
 SELECT t.title
 	FROM public.tracks t
 	LEFT JOIN collectionstracks ct ON t.id = ct.track_id 
 	WHERE ct.track_id IS null;
 
+-- 4.3
 SELECT a.name
 	FROM public.artists a
 	JOIN artistsalbum aa ON a.id = aa.artist_id 
@@ -74,6 +87,7 @@ SELECT a.name
 	JOIN tracks t ON al.id = t.album_id 
 	WHERE t.duration = (SELECT MIN(duration) FROM tracks);
 
+-- 4.4
 SELECT al.title, COUNT(*)
 	FROM public.albums al
 	JOIN tracks t ON al.id = t.album_id
