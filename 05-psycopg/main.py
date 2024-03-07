aaa8b846975c559961631d23317cf308c10a05a0
XFinity <x.finity777@gmail.com>
2024-03-07 22:15:25 +0300

    add add_new_client, add_new_phone, modify_client functions

--- 05-psycopg/main.py
+++ 05-psycopg/main.py
@@ -2,9 +2,13 @@ import xf_DB as xfdb
 import psycopg2
 import json
 
-config = xfdb.load_config('config.json')
-conn = psycopg2.connect(database=config['database'], user=config['user'], password=config['password'])
-with conn.cursor() as cur:
-    xfdb.create_client_db(conn, cur)
+if __name__ == "__main__":
+    config = xfdb.load_config('config.json')
+    conn = psycopg2.connect(database=config['database'], user=config['user'], password=config['password'])
+    with conn.cursor() as cur:
+        xfdb.create_client_db(conn, cur)
+        xfdb.add_new_client(conn, cur, 'NoName', 'test@domain.lol', '123456789')
+        xfdb.add_new_phone(conn, cur, 'NoName', '123456789')
+        xfdb.modify_client(conn, cur, 'NoName')
 
-conn.close()
+    conn.close()
--- 05-psycopg/xf_DB.py
+++ 05-psycopg/xf_DB.py
@@ -16,3 +16,40 @@ def create_client_db(connection, cursor):
         client_id INTEGER REFERENCES client(id),
         phone VARCHAR(40) UNIQUE)''')
     connection.commit()
+
+def add_new_client(connection, cursor, name, email, phone):
+    try:
+        cursor.execute('''INSERT INTO client(name, email) VALUES (%s, %s)''', (name, email))
+    except psycopg2.errors.UniqueViolation:
+        connection.rollback()
+        print('Client already exists')
+        return
+    client_id = cursor.fetchone()[0]
+    cursor.execute('''INSERT INTO phones(client_id, phone) VALUES (%s, %s)''', (client_id, phone))
+    connection.commit()
+    return client_id
+
+def add_new_phone(connection, cursor, client_name, phone):
+    cursor.execute('''SELECT id FROM client WHERE name = %s''', (client_name,))
+    client_id = cursor.fetchone()[0]
+    try:
+        cursor.execute('''INSERT INTO phones(client_id, phone) VALUES (%s, %s)''', (client_id, phone))
+    except psycopg2.errors.UniqueViolation:
+        connection.rollback()
+        print('Phone already exists')
+        return
+    connection.commit()
+
+def modify_client(connection, cursor, client_name, name=None, email=None, phone=None):
+    if not name and not email and not phone:
+        print('No input data')
+        return
+    cursor.execute('''SELECT id FROM client WHERE name = %s''', (client_name,))
+    client_id = cursor.fetchone()[0]
+    if name:
+        cursor.execute('''UPDATE client SET name = %s WHERE id = %s''', (name, client_id))
+    if email:
+        cursor.execute('''UPDATE client SET email = %s WHERE id = %s''', (email, client_id))
+    if phone:
+        cursor.execute('''UPDATE phones SET phone = %s WHERE client_id = %s''', (phone, client_id))
+    connection.commit()
