264f83cfd9b067de791bdee07a018715ebeafe24
XFinity <x.finity777@gmail.com>
2024-03-12 01:44:11 +0300

    fill_db

---
+++ 06-orm/main.py
@@ -0,0 +1,18 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+import xf_orm as xform
+
+config = xform.load_config('config.json')
+print(config)
+DSN = f'postgresql://{config["user"]}:{config["password"]}@{config["server"]}:{config["port"]}/{config["database"]}'
+
+engine = sq.create_engine(DSN)
+
+xform.create_db(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+xform.fill_db(session)
+
+session.commit()
